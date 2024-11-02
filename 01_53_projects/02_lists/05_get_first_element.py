cd8076091921e29f4da9fd416b148faad7f410b0
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-03 03:40:01 +0500

    initial commit

---
+++ 01_53_projects/02_lists/05_get_first_element.py
@@ -0,0 +1,11 @@
+def get_first_element(Ist):
+    first_element = Ist[0]
+    return first_element
+
+def element():
+    Ist = [1,2,4,"khurram",18]
+    getting_first_element = get_first_element(Ist)
+    print(getting_first_element)
+
+if __name__ == "__main__":
+    element()
