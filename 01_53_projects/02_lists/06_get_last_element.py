34ccbdd69002ff03b72d0b29f1dd5e08a13ea19c
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-03 03:41:45 +0500

    Chore

---
+++ 01_53_projects/02_lists/06_get_last_element.py
@@ -0,0 +1,11 @@
+def get_last_element(x):
+    last_element = x[-1]
+    return last_element
+
+def list():
+    last = [89,"lahore",57,45,"khurram"]
+    getting_last_element = get_last_element(last)
+    print(getting_last_element)
+
+if __name__ == "__main__":
+    list()
