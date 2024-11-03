314ad3bda608ef7aa0c864de5268d8f0457a32b5
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-04 04:25:01 +0500

    data types

---
+++ 03_Python_Revision/02_Primitive_datatype/int.py
@@ -0,0 +1,23 @@
+# INTEGER DATA TYPE 
+# EVERY NUMBER IS CALL A INTEGER 
+# EXAMPLE
+number :int = 78
+print(type(number))
+even_numbers = 2,4,6,8,10
+print(even_numbers)
+
+
+# FLOAT DATA TYPE 
+# FLOAT IS CALLED WHEN A NUMBER CONTAIN DECIMAL POINT IN IT 
+# EXAMPLE 
+
+num : float = 12.5
+print(type(num))
+
+# COMPLEX NUMBERS
+
+#  COMPLEX NUMBER IS A NUMBER CONSIST OF REAL AND IMAGINARY PART 
+# EXAMPLE 
+
+value : complex = 3 + 2j
+print(type(value))
