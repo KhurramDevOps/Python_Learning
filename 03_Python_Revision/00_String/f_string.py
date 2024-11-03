21a2d8b8079034d226e5d4ce38f465ddccfafbb4
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-04 04:15:54 +0500

    f String concept

---
+++ 03_Python_Revision/00_String/f_string.py
@@ -0,0 +1,18 @@
+# f string 
+
+# f string stand for formatted string 
+
+# example with f string  
+
+name = "Khurram"
+
+age = 18
+
+email = "Khurramshahzad2440@gamil.com"
+
+print(f"my name is {name} my age is {age} and my email is {email}")
+
+#  example without f string 
+
+print("my name is",name, "my age is" ,age , "my email is", email)
+
