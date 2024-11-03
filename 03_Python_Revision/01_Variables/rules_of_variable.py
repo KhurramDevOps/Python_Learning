f0eccd2a13b81338643a9f3d2b5124d677d8b9d0
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-04 04:23:40 +0500

    variable

---
+++ 03_Python_Revision/01_Variables/rules_of_variable.py
@@ -0,0 +1,45 @@
+# what is variable 
+# variable is used to store any type of data in it for example u have a box and u put some boots in it in this the Box is a Variable and the boots are the data which is stored in a variable 
+
+# example 
+name = "My name is Khurram"
+fridge = "couple of things"
+
+# here name is a variable and the data stored in it is after the = sign 
+
+# Constant Variabe 
+# to make variable constant theirs a way in which the variable name is in upper case and u cant change it python allows us to change but we dont have to change
+# example 
+
+NAME = "khurram"
+AGE = 18
+HEIGHT = 5.9
+# the above is the constant variable 
+
+
+# RULES OF NAMING A VARIABLE
+# 1 . THE VARIABLE NAME CANT START WITH NUMBER
+# 2 . THE VARIABLE NAME DOESN'T CONTAIN SPACE
+# 3 . THE VARIABLE NAME CANT USE PREDEFINED WORDS 
+# 4 . THE VARIABLE NAME DONT HAVE A SPECIAL CHARACTER 
+
+# 1 example 
+2_name = "khurram"
+34_class= "oop"
+
+# 2 example
+my age = 18 
+my name = "khurram"
+
+# 3 example 
+print = "hello how are you "
+# its shows error cuz print is predefined 
+
+# 4 example
+sign& = "its also give error "
+scores @ = 233
+
+# TASK
+my_name = "khurram"
+info = "i'm a student of Piaic " 
+print(f"my name is {my_name} and {info}")              
