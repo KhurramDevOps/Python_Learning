8bf9eaf14047a5bc363735da285e0595565c2784
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-04 04:20:17 +0500

    initial commit

---
+++ 03_Python_Revision/escape_sequence.py
@@ -0,0 +1,21 @@
+# escape sequences
+# \n ,\t are the escape sequences we use 
+
+# \n 
+# \n is placed to give the statement in the new line
+
+print ("my\nname\nis\khurram")
+
+# \t
+# \t is used to give the space its work like tab
+
+print("hello\thow\tare\tyou")
+
+# \
+# its also used to make to print double quotation in the string using double quatation
+
+print("someone said: \"hello\"")
+print('someone said: \'bye\'')
+
+# TASK 
+print("someone said \n\t\t\"ali\" is a 'good' boy \n\tbut \nhe often make mistakes ")
