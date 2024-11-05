3dbc02c9229c37784dcfb63b26888b3955c36fa3
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-06 01:28:43 +0500

    end sep functionality

---
+++ 03_Python_Revision/parameters_in_print_function.py
@@ -0,0 +1,15 @@
+# end parameter 
+# end parameter let u control what appear at the end of the print statement
+#  by default print statement end after the statement
+print("welcome to ", end = " ")
+print("the party")
+
+
+# sep parameter 
+# sep means seperator 
+# specifies the seperator between multiple objects
+print ("khurram" ,"is" ,"student " , sep = "_")
+
+print("helle","bye","hy", sep = "\n")
+# sep parameter let u control what appear between the print statement
+# by default print statement use space as a seperator
