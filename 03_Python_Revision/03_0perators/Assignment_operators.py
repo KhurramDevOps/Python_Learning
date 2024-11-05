083238886b17470ee261b4b9506420bc95fad95a
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-05 22:26:03 +0500

    assignment operators

---
+++ 03_Python_Revision/03_0perators/Assignment_operators.py
@@ -0,0 +1,26 @@
+# Assignment operators 
+# =, +=, -=, *=, /=, %=, **=, //=, <<=,
+# >>=, >>>=, &=, ^=, |=
+
+# example 
+x = 5
+x = x + 5
+print(x)  # prints 10
+x += 5
+print(x)  # prints 15
+x -= 5
+print(x)  # prints 10
+x *= 5
+print(x)  # prints 50
+x /= 5
+print(x)  # prints 10.0
+x %= 5
+print(x)  # prints 0
+x **= 5
+print(x)  # prints 0
+x //= 5
+print(x)  # prints 0
+x <<= 5
+print(x)  # prints 0
+x >>= 5
+print(x)  # prints 0
