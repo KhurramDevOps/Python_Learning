82cb83808d3fb9c8e6b77e0fa74d059d277901fe
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-05 22:29:56 +0500

    rational

---
+++ 03_Python_Revision/03_0perators/Rational_operators.py
@@ -0,0 +1,36 @@
+
+# RELATIONAL _OPERATORS 
+
+# 1. Equal to (==)
+# 2. Not Equal to (!=)  
+# 3. Greater than (>)
+# 4. Less than (<)
+# 5. Greater than or equal to (>=)
+# 6. Less than or equal to (<=)
+
+# EXAMPLE OF CODE 
+age = 25
+if age == 25:
+    print("Age is 25")
+else:
+    print("Age is not 25")
+
+ali_age = 45
+amir_age = 43
+
+print(ali_age > amir_age)
+
+a = 10
+b = 5
+print(a > b)  
+
+a = 10
+b = 10
+print(a >= b)  
+
+a = 5
+b = 10
+print(a != b)  
+
+
+
