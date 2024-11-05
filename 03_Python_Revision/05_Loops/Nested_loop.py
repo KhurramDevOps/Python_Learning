36d911af6d4f404f12e27146eab7f83ffaf0d17c
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-06 01:36:27 +0500

    Nested loop

---
+++ 03_Python_Revision/05_Loops/Nested_loop.py
@@ -0,0 +1,10 @@
+# NESTED LOOP 
+# what is  a nested loop? 
+# a nested loop is a loop inside another loop
+# it is used to iterate over multiple lists or arrays at the same time
+
+# EXAMPLE 
+
+for x in range(1,4):
+    for y in range(1,4):
+        print(x,y)
