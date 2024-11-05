5169fca1de3d7703ca86fd5dddf92f336f2db02e
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-05 22:31:41 +0500

    conditions

---
+++ 03_Python_Revision/04_if_Statements/if.py
@@ -0,0 +1,25 @@
+#  IF STATEMENTS 
+# what is if_else  statement
+# if_else statement is used to execute a block of code if a certain condition is met
+# it is used to make decisions in the program
+
+# Example  
+
+i = 10 
+if (i != 10) :
+    print("i is equal to 10")
+else:
+    print("i is not equal to 10")
+
+print("outside if")
+
+x = 25
+if x == 10:
+    print("x is 10")
+elif x == 15:
+    print("x is 15")
+elif x == 25:
+    print("x is 25")
+else :
+    print("i is not present")
+
