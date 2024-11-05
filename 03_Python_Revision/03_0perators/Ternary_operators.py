4818c8b85a58b2d8ef9e9edb5a6c1ac2f609e8a7
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-05 22:30:22 +0500

    ternary

---
+++ 03_Python_Revision/03_0perators/Ternary_operators.py
@@ -0,0 +1,20 @@
+# TERNARY OPERATORS 
+#  what are ternary operators?
+#  ternary operators are a shorthand way of writing if-else statements in a single line of
+#  code. They are called ternary because they take three arguments: a condition, a value
+
+# Example 
+
+x = 5
+y = 10
+result = "x is greater than y" if x > y else "x is less than or equal to y"
+print(result)
+
+
+age = 20
+eligibility = "Eligible" if age >= 18 else "Not eligible"
+print(eligibility)
+
+number = 7
+result = "Even" if number % 2 == 0 else "Odd"
+print(result)
