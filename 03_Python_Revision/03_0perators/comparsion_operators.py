0e42c4ff5b158cbdd791454aad727b5dd52938d5
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-05 22:27:27 +0500

    comparison operators

---
+++ 03_Python_Revision/03_0perators/comparsion_operators.py
@@ -0,0 +1,13 @@
+# COMPARISON OPERATORS 
+
+# ================
+# Equal to
+# ================
+a = 5
+b = 5
+print(a == b)  # True
+print(a != b)  # False
+print(a == 5)   # True
+print(a != 5)   # False
+print(a == 10)  # False
+print(a != 10)  # True
