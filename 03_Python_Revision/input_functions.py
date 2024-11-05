2fdbbc7a4b8a0ee4a1ccf28e706563d09451672d
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-06 01:25:39 +0500

    taking input

---
+++ 03_Python_Revision/input_functions.py
@@ -0,0 +1,19 @@
+#  INPUT FUNCTION
+# how to take user_input
+
+name = input("Write your name : ")
+print("your name is : ", + name )
+
+num = input("Write a number between 1 to 10 :")
+print(num)
+
+#  INPUT FUNCTION RETURN STR 
+
+age = input("write your age : ")
+print("your age is : ", + age)
+print(type(age))
+
+#  TO CONVERT THE RETURN TYPE 
+
+age_int = int(age)
+print(type(age_int))
