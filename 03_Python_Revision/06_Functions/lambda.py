f9c8d5c77ec17ae5db7ce8514c479bb09c826b43
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-07 22:33:56 +0500

    lambda function

---
+++ 03_Python_Revision/06_Functions/lambda.py
@@ -0,0 +1,12 @@
+# LAMBDA FUNCTION 
+# Its a short hand function
+# A lambda function is a small anonymous function that can take any number of arguments, but can only
+# have one expression.
+# Syntax: lambda arguments : expression
+
+add =  lambda x, y : x + y
+print(add(2,3))
+
+square = lambda x :  x ** 2
+print(square(5))
+
