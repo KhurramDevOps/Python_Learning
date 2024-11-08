3696233a5cde94f35d69347cbdefbfbf405d9f79
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-08 19:02:47 +0500

    tuple methods

---
+++ 03_Python_Revision/08_tuple/tuple_functions.py
@@ -0,0 +1,14 @@
+# TUPLE FUNCTIONS 
+
+# 1.  count() :  This function returns the number of elements with the specified value.
+# EXAMPLE :
+
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1.count(4)) 
+
+# 2.  index() :  This function returns the index of the first occurrence of the specified value.
+# EXAMPLE :
+
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1.index(4))
+
