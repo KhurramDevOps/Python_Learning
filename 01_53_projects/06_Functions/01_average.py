3884da1730e64ff932279cf64fde3130f4551e04
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-11-12 01:27:53 +0500

    projects using functions

---
+++ 01_53_projects/06_Functions/01_average.py
@@ -0,0 +1,10 @@
+def average(x : float,y : float ):
+    """ Returns the number between x and y 
+    """
+    add = x + y
+    divide = add / 2
+    print(divide)
+
+if __name__ == "__main__":
+    average(12,34)
+
